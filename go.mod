a2ac044cdde907b72d4e6fee9cc1ff9b81b6eba6
thloyi <tfspq@protonmail.com>
2023-04-03 10:26:53 +0800

    trace init

--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.16
 
 require (
 	github.com/go-kratos/kratos/v2 v2.6.1
-	go.opentelemetry.io/otel/sdk v1.7.0
-	go.opentelemetry.io/otel/trace v1.7.0
+	go.opentelemetry.io/otel/sdk v1.14.0
+	go.opentelemetry.io/otel/trace v1.14.0
 )
 
 require (
-	github.com/go-logr/logr v1.2.3 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	go.opentelemetry.io/otel v1.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	go.opentelemetry.io/otel v1.14.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.14.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.14.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.14.0
 )
---
+++ trace.go
@@ -0,0 +1,81 @@
+package golog
+
+import (
+	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"os"
+	"strings"
+)
+
+const (
+	OTLPProtocol    = "OTEL_EXPORTER_OTLP_PROTOCOL"
+	OTLPEndpoint    = "OTEL_EXPORTER_OTLP_ENDPOINT"
+	OTLPCompression = "OTEL_EXPORTER_OTLP_COMPRESSION"
+	OTLPHeaders     = "OTEL_EXPORTER_OTLP_HEADERS"
+	OTLPPath        = "OTEL_EXPORTER_OTLP_PATH"
+)
+
+// InitOTLPTracer init export by env
+// headers example: x-otel-project=,x-otel-access-id=,x-otel-access-key=
+func InitOTLPTracer(serverName string, ratio float64) error {
+	protocol := os.Getenv(OTLPProtocol)
+	host := os.Getenv(OTLPEndpoint)
+	headersEnv := os.Getenv(OTLPHeaders)
+	headers := make(map[string]string)
+	for _, pairs := range strings.Split(headersEnv, ",") {
+		s := strings.Split(pairs, "=")
+		if len(s) != 2 {
+			continue
+		}
+		headers[strings.TrimSpace(s[0])] = strings.TrimSpace(s[1])
+	}
+	var (
+		exp *otlptrace.Exporter
+		err error
+	)
+	if protocol == "grpc" {
+		options := []otlptracegrpc.Option{
+			otlptracegrpc.WithEndpoint(host),
+			otlptracegrpc.WithHeaders(headers),
+		}
+		compression := os.Getenv(OTLPCompression)
+		if compression == "gzip" {
+			options = append(options, otlptracegrpc.WithCompressor(compression))
+		}
+		exp, err = otlptracegrpc.New(context.Background(), options...)
+
+	} else if protocol == "http" {
+		options := []otlptracehttp.Option{
+			otlptracehttp.WithEndpoint(host),
+			otlptracehttp.WithHeaders(headers),
+		}
+		compression := os.Getenv(OTLPCompression)
+		if compression == "gzip" {
+			options = append(options, otlptracehttp.WithCompression(otlptracehttp.GzipCompression))
+		}
+		path := os.Getenv(OTLPPath)
+		if path != "" {
+			options = append(options, otlptracehttp.WithURLPath(path))
+		}
+		exp, err = otlptracehttp.New(context.Background(), options...)
+	}
+	if err != nil {
+		return err
+	}
+
+	tp := tracesdk.NewTracerProvider(
+		tracesdk.WithSampler(tracesdk.ParentBased(tracesdk.TraceIDRatioBased(ratio))),
+		tracesdk.WithBatcher(exp),
+		tracesdk.WithResource(resource.NewSchemaless(
+			semconv.ServiceNameKey.String(serverName),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+	return nil
+}
